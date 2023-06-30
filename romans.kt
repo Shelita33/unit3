737b5c6867db7194c2ba7161cc540e097168b8e1
Shelita33 <135672845+Shelita33@users.noreply.github.com>
2023-06-30 15:57:59 -0500

    romans.kt

---
+++ romans.kt
@@ -0,0 +1,13 @@
+fun main() {
+    when (1) {
+    in 1..10 -> println("The number is between 1 and 10")
+    else -> println("Error")
+}
+val number = 1/10
+
+if (number > 1/10) {
+    println("$number is positive")
+} else {
+    println("$number is error")
+}
+}
